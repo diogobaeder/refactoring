2f090663f26bfc29876e93a59d6a7e3f5675a33e
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2012-11-11 22:28:55 +0000

    Melhorando exemplo do extract class

--- refactoring/extractclass.py
+++ refactoring/extractclass.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
 class UserRepository(object):
+    def __init__(self):
+        self._fake_database = []
+
     def find_active_users(self):
         active_users_data = self._find_in_database('active', True)
         return [User(user['name'], True) for user in active_users_data]
 
+    def insert_users(self, users):
+        self._fake_database.extend(users)
+
     def _find_in_database(self, constraint, value):
         # Vamos fingir que isto é uma query real em banco
-        fake_data = [
-            {
-                'name': 'Nat Pryce',
-                'active': True,
-            },
-            {
-                'name': 'Steve Freeman',
-                'active': True,
-            },
-            {
-                'name': 'Alfred Hitchcock',
-                'active': False,
-            },
-        ]
-        return [user for user in fake_data if user[constraint] == value]
+        return [user for user in self._fake_database
+                if user[constraint] == value]
 
 
 class User(object):
--- test/test_extractclass.py
+++ test/test_extractclass.py
@@ -5,10 +5,28 @@ from refactoring.refactoring.extractclass import UserRepository
 
 
 class UserRepositoryTest(TestCase):
+    def populate_database(self, repository):
+        repository.insert_users([
+            {
+                'name': 'Nat Pryce',
+                'active': True,
+            },
+            {
+                'name': 'Steve Freeman',
+                'active': True,
+            },
+            {
+                'name': 'Alfred Hitchcock',
+                'active': False,
+            },
+        ])
+
     @istest
     def finds_active_users(self):
         repository = UserRepository()
+        self.populate_database(repository)
 
         users = sorted(repository.find_active_users())
 
+        self.assertEqual(len(users), 2)
         self.assertTrue(all(user.active for user in users))
